a87a7ca2bc3ede4d370bdb201e75c08c832aa759
Jack Shunn <jack.shunn@gmail.com>
2023-10-06 17:43:09 -0600

    boilerplate for the 4 routes: contact, goals, and officers

--- index.ts
+++ index.ts
@@ -8,3 +8,31 @@ Bun.serve({
       },});
     },
   });
+
+  Bun.serve({
+    port: 5500, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
+    //hostname: "mydomain.com", // defaults to "0.0.0.0"
+    async fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/") 
+        return new Response(
+            await Bun.file("index.html").text(), {
+                headers: {
+                "Content-Type": "text/html",
+              },}
+        );
+        if (url.pathname === "/goals") 
+        return new Response(
+            await Bun.file("goals.html").text(), {
+            });
+        if (url.pathname === "/officers") 
+        return new Response(
+            await Bun.file("officers.html").text(), {
+            });
+        if (url.pathname === "/contact") 
+        return new Response(
+            await Bun.file("contact.html").text(), {
+            });
+        return new Response("404");
+    }
+  });
