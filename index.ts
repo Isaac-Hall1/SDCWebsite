8d2b8d8ff45711c76a8167e9fdd238d1f5086057
Jack Shunn <jack.shunn@gmail.com>
2023-10-17 01:29:47 -0600

    Revert "format index.ts to match eslint style guide"
    This reverts commit 2b99cc54f8f7e0b5a2207a4b59f050571a7d1f2c.

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-const port: number = 5500
-const router = new Bun.FileSystemRouter({
-  dir: process.cwd() + '/content',
-  style: 'nextjs',
-  fileExtensions: ['.html']
-})
+const port: number = 5500;
+let router = new Bun.FileSystemRouter({
+    dir: process.cwd() + "/content",
+    style: "nextjs",
+    fileExtensions:[".html"]
+});
 
-console.log(`Starting server on port ${port} (http://localhost:${port}/)`)
+console.log(`Starting server on port ${port} (http://localhost:${port}/)`);
 
 Bun.serve({
-  port,
-  async fetch (req) {
-    const url: URL = new URL(req.url)
-    const file: string | undefined = router.routes[url.pathname]
+    port,
+    async fetch(req) {
+        const url: URL = new URL(req.url);
+        let file: string | undefined = router.routes[url.pathname]
 
-    if (file !== undefined) {
-      return new Response(await Bun.file(file).text(), {
-        headers: { 'Content-Type': 'text/html' }
-      })
+        if(file){
+            return new Response(await Bun.file(file).text(), {
+                headers: { "Content-Type": "text/html" }
+            });
+        }
+        return new Response("<p><b>404 Error</b></p>", {
+            headers: { "Content-Type": "text/html" }
+        });
     }
-    return new Response('<p><b>404 Error</b></p>', {
-      headers: { 'Content-Type': 'text/html' }
-    })
-  }
-})
+});
